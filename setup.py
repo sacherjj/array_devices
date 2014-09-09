bb844a603ab7c1873e7b8f1575f5855c24a21eff
sacherjj <sacherjj@gmail.com>
2014-09-09 18:18:42 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,45 @@
+
+from __future__ import with_statement
+
+# http://docs.python.org/distutils/
+# http://packages.python.org/distribute/
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+import os.path
+
+version = os.path.join(os.path.dirname(__file__), 'VERSION')
+with open(version, 'r') as f:
+    version = f.readline().rstrip()
+
+setup(
+    name = 'array_devices',
+    description = 'Python Interface for Array Electronic Load',
+    version = version,
+    long_description = '''This package is a Python-based interface for the Array 3710 Electronic Load.''',
+    author = 'Joe Sacher',
+    author_email = 'sacherjj@gmail.com',
+    url = 'https://github.com/sacherjj/array_devices',
+    download_url = 'http://github.com/sacherjj/array_devices/tarball/master',
+    keywords = 'Array Electronic Load',
+    license = 'MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+        ],
+    packages = ['array_devices'],
+    requires = [],
+    extras_require = {
+        'serial': ['pyserial']
+    }
+)
